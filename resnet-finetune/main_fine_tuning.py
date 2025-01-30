744e5f45fb8fdb750d9a4d5a9c8713b098354d4c
Joaquin Negrete <joaquin.negrete03@gmail.com>
2025-01-30 18:00:50 +0100

    Fix bug in saving model

--- resnet-finetune/main_fine_tuning.py
+++ resnet-finetune/main_fine_tuning.py
@@ -1,6 +1,7 @@
-### Section 1 - First, let's import everything we will be needing.
+# Section 1 - First, let's import everything we will be needing.
 
 from __future__ import print_function, division
+from fine_tuning_config_file import *
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -14,9 +15,8 @@ import copy
 import os
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-from fine_tuning_config_file import *
 
-## If you want to keep a track of your network on tensorboard, set USE_TENSORBOARD TO 1 in config file.
+# If you want to keep a track of your network on tensorboard, set USE_TENSORBOARD TO 1 in config file.
 
 if USE_TENSORBOARD:
     from pycrayon import CrayonClient
@@ -28,27 +28,27 @@ if USE_TENSORBOARD:
     foo = cc.create_experiment(EXP_NAME)
 
 
-## If you want to use the GPU, set GPU_MODE TO 1 in config file
+# If you want to use the GPU, set GPU_MODE TO 1 in config file
 
 use_gpu = GPU_MODE
 if use_gpu:
     torch.cuda.set_device(CUDA_DEVICE)
 
-count=0
+count = 0
 
-### SECTION 2 - data loading and shuffling/augmentation/normalization : all handled by torch automatically.
+# SECTION 2 - data loading and shuffling/augmentation/normalization : all handled by torch automatically.
 
 # This is a little hard to understand initially, so I'll explain in detail here!
 
-# For training, the data gets transformed by undergoing augmentation and normalization. 
+# For training, the data gets transformed by undergoing augmentation and normalization.
 # The RandomSizedCrop basically takes a crop of an image at various scales between 0.01 to 0.8 times the size of the image and resizes it to given number
 # Horizontal flip is a common technique in computer vision to augment the size of your data set. Firstly, it increases the number of times the network gets
 # to see the same thing, and secondly it adds rotational invariance to your networks learning.
 
 
-# Just normalization for validation, no augmentation. 
+# Just normalization for validation, no augmentation.
 
-# You might be curious where these numbers came from? For the most part, they were used in popular architectures like the AlexNet paper. 
+# You might be curious where these numbers came from? For the most part, they were used in popular architectures like the AlexNet paper.
 # It is important to normalize your dataset by calculating the mean and standard deviation of your dataset images and making your data unit normed. However,
 # it takes a lot of computation to do so, and some papers have shown that it doesn't matter too much if they are slightly off. So, people just use imagenet
 # dataset's mean and standard deviation to normalize their dataset approximately. These numbers are imagenet mean and standard deviation!
@@ -70,22 +70,21 @@ data_transforms = {
 }
 
 
-
 # Enter the absolute path of the dataset folder below. Keep in mind that this code expects data to be in same format as Imagenet. I encourage you to
 # use your own dataset. In that case you need to organize your data such that your dataset folder has EXACTLY two folders. Name these 'train' and 'valid'
-# Yes, this is case sensitive. The 'train' folder contains training set and 'valid' fodler contains validation set on which accuracy is measured. 
+# Yes, this is case sensitive. The 'train' folder contains training set and 'valid' fodler contains validation set on which accuracy is measured.
 
-# The structure within 'train' and 'valid' folders will be the same. They both contain one folder per class. All the images of that class are inside the 
+# The structure within 'train' and 'valid' folders will be the same. They both contain one folder per class. All the images of that class are inside the
 # folder named by class name.
 
 # So basically, if your dataset has 3 classes and you're trying to classify between pictures of 1) dogs 2) cats and 3) humans,
-# say you name your dataset folder 'data_directory'. Then inside 'data_directory' will be 'train' and 'test'. Further, Inside 'train' will be 
+# say you name your dataset folder 'data_directory'. Then inside 'data_directory' will be 'train' and 'test'. Further, Inside 'train' will be
 # 3 folders - 'dogs', 'cats', 'humans'. All training images for dogs will be inside this 'dogs'. Similarly, within 'valid' as well there will be the same
-# 3 folders. 
+# 3 folders.
 
-## So, the structure looks like this : 
+# So, the structure looks like this :
 # data_dar
-#      |- train 
+#      |- train
 #            |- dogs
 #                 |- dog_image_1
 #                 |- dog_image_2
@@ -111,26 +110,26 @@ dset_sizes = {x: len(dsets[x]) for x in ['train', 'valid']}
 dset_classes = dsets['train'].classes
 
 
-### SECTION 3 : Writing the functions that do training and validation phase. 
+# SECTION 3 : Writing the functions that do training and validation phase.
 
 # These functions basically do forward propogation, back propogation, loss calculation, update weights of model, and save best model!
 
 
-## The below function will train the model. Here's a short basic outline - 
+# The below function will train the model. Here's a short basic outline -
 
-# For the number of specified epoch's, the function goes through a train and a validation phase. Hence the nested for loop. 
+# For the number of specified epoch's, the function goes through a train and a validation phase. Hence the nested for loop.
 
-# In both train and validation phase, the loaded data is forward propogated through the model (architecture defined ahead). 
-# In PyTorch, the data loader is basically an iterator. so basically there's a get_element function which gets called everytime 
-# the program iterates over data loader. So, basically, get_item on dset_loader below gives data, which contains 2 tensors - input and target. 
+# In both train and validation phase, the loaded data is forward propogated through the model (architecture defined ahead).
+# In PyTorch, the data loader is basically an iterator. so basically there's a get_element function which gets called everytime
+# the program iterates over data loader. So, basically, get_item on dset_loader below gives data, which contains 2 tensors - input and target.
 # target is the class number. Class numbers are assigned by going through the train/val folder and reading folder names in alphabetical order.
 # So in our case cats would be first, dogs second and humans third class.
 
-# Forward prop is as simple as calling model() function and passing in the input. 
+# Forward prop is as simple as calling model() function and passing in the input.
 
 # Variables are basically wrappers on top of PyTorch tensors and all that they do is keep a track of every process that tensor goes through.
 # The benefit of this is, that you don't need to write the equations for backpropogation, because the history of computations has been tracked
-# and pytorch can automatically differentiate it! Thus, 2 things are SUPER important. ALWAYS check for these 2 things. 
+# and pytorch can automatically differentiate it! Thus, 2 things are SUPER important. ALWAYS check for these 2 things.
 # 1) NEVER overwrite a pytorch variable, as all previous history will be lost and autograd won't work.
 # 2) Variables can only undergo operations that are differentiable.
 
@@ -147,17 +146,17 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=100):
         # Each epoch has a training and validation phase
         for phase in ['train', 'valid']:
             if phase == 'train':
-                mode='train'
+                mode = 'train'
                 optimizer = lr_scheduler(optimizer, epoch)
                 model.train()  # Set model to training mode
             else:
                 model.eval()
-                mode='valid'
+                mode = 'valid'
 
             running_loss = 0.0
             running_corrects = 0
 
-            counter=0
+            counter = 0
             # Iterate over data.
             for data in dset_loaders[phase]:
                 inputs, labels = data
@@ -165,10 +164,10 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=100):
                 # wrap them in Variable
                 if use_gpu:
                     try:
-                        inputs, labels = Variable(inputs.float().cuda()),                             
+                        inputs, labels = Variable(inputs.float().cuda()),
                         Variable(labels.long().cuda())
                     except:
-                        print(inputs,labels)
+                        print(inputs, labels)
                 else:
                     inputs, labels = Variable(inputs), Variable(labels)
 
@@ -176,13 +175,13 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=100):
                 optimizer.zero_grad()
                 outputs = model(inputs)
                 _, preds = torch.max(outputs.data, 1)
-                
+
                 loss = criterion(outputs, labels)
-                # print('loss done')                
+                # print('loss done')
                 # Just so that you can keep track that something's happening and don't feel like the program isn't running.
                 # if counter%10==0:
                 #     print("Reached iteration ",counter)
-                counter+=1
+                counter += 1
 
                 # backward + optimize only if in training phase
                 if phase == 'train':
@@ -207,16 +206,15 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=100):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-
             # deep copy the model
             if phase == 'valid':
                 if USE_TENSORBOARD:
-                    foo.add_scalar_value('epoch_loss',epoch_loss,step=epoch)
-                    foo.add_scalar_value('epoch_acc',epoch_acc,step=epoch)
+                    foo.add_scalar_value('epoch_loss', epoch_loss, step=epoch)
+                    foo.add_scalar_value('epoch_acc', epoch_acc, step=epoch)
                 if epoch_acc > best_acc:
                     best_acc = epoch_acc
                     best_model = copy.deepcopy(model)
-                    print('new best accuracy = ',best_acc)
+                    print('new best accuracy = ', best_acc)
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
@@ -225,6 +223,8 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=100):
     return best_model
 
 # This function changes the learning rate over the training model.
+
+
 def exp_lr_scheduler(optimizer, epoch, init_lr=BASE_LR, lr_decay_epoch=EPOCH_DECAY):
     """Decay learning rate by a factor of DECAY_WEIGHT every lr_decay_epoch epochs."""
     lr = init_lr * (DECAY_WEIGHT**(epoch // lr_decay_epoch))
@@ -238,7 +238,7 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=BASE_LR, lr_decay_epoch=EPOCH_DEC
     return optimizer
 
 
-### SECTION 4 : DEFINING MODEL ARCHITECTURE.
+# SECTION 4 : DEFINING MODEL ARCHITECTURE.
 
 # We use Resnet18 here. If you have more computational power, feel free to swap it with Resnet50, Resnet100 or Resnet152.
 # Since we are doing fine-tuning, or transfer learning we will use the pretrained net weights. In the last line, the number of classes has been specified.
@@ -246,7 +246,7 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=BASE_LR, lr_decay_epoch=EPOCH_DEC
 
 model_ft = models.resnet18(pretrained=True)
 
-model_ft.save_state_dict('../model/classification/pretrained_model.pt')
+model_ft._save_to_state_dict('../model/classification/pretrained_model.pt')
 
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = nn.Linear(num_ftrs, NUM_CLASSES)
@@ -261,10 +261,9 @@ if use_gpu:
 optimizer_ft = optim.RMSprop(model_ft.parameters(), lr=0.0001)
 
 
-
 # Run the functions and save the best model in the function model_ft.
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=100)
 
 # Save model
-model_ft.save_state_dict('../trained_model/classification/fine_tuned_best_model.pt')
+model_ft._save_to_state_dict('../trained_model/classification/fine_tuned_best_model.pt')
